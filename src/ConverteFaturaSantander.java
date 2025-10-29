c92bb6cd9dd2187c713cea5192034d71e5fc9b58
Henrique Mendes <henrique1258@gmail.com>
2025-10-29 18:01:24 -0300

    atualizando repositório

--- src/ConverteFaturaSantander.java
+++ src/ConverteFaturaSantander.java
@@ -7,11 +7,19 @@ import java.util.Date;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.Calendar;
 
 public class ConverteFaturaSantander {
+
     public static void main(String[] args) {
         List<FaturaSantander> faturaList = new ArrayList<>();
         String path = "C:\\Users\\henrique.r.mendes\\Documentos\\conversor-fatura\\src\\faturaSantander.txt";
+
+        // Obtém o ano e mês atuais
+        Calendar cal = Calendar.getInstance();
+        int anoAtual = cal.get(Calendar.YEAR);
+        int mesAtual = cal.get(Calendar.MONTH) + 1; // Mês é de 0 a 11
+
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(path))) {
             String faturaCsv = bufferedReader.readLine();
             while (faturaCsv != null) {
@@ -25,7 +33,14 @@ public class ConverteFaturaSantander {
                     String nome = matcher.group(3).trim();
                     String parcelas = (matcher.group(4) == null || matcher.group(4).isEmpty()) ? "N/A" : matcher.group(4).replace("/", "|");
                     String valor = matcher.group(5);
-                    faturaList.add(new FaturaSantander(tipo, data, nome, parcelas, valor));
+
+                    // Extrai o mês da data da transação
+                    int mesTransacao = Integer.parseInt(data.substring(3, 5));
+
+                    // Adiciona o ano à data da transação
+                    String dataCompleta = data + "/" + (mesTransacao > mesAtual ? anoAtual - 1 : anoAtual);
+
+                    faturaList.add(new FaturaSantander(tipo, dataCompleta, nome, parcelas, valor));
                 } else {
                     System.out.println("A linha não corresponde ao padrão esperado.");
                     System.out.println("Linha: " + faturaCsv);
@@ -39,7 +54,7 @@ public class ConverteFaturaSantander {
         SimpleDateFormat sdf = new SimpleDateFormat("dd_MM_yyyy__HH_mm_ss");
         Date date = new Date();
         String arquivoSaida = "C:\\Users\\henrique.r.mendes\\Documentos\\conversor-fatura\\src\\faturaSantanderConvertida_" + sdf.format(date) + ".csv";
-        //mesmo procedimento de leitura de arquivos, o parâmetro true no FileWriter significa que vai definir como append
+
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(arquivoSaida, true))) {
             bufferedWriter.write("tipoCompra;dataCompra;nomeLocal;numParcelas;valorCompra\n");
             for (FaturaSantander fatura : faturaList) {
